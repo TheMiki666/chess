7034420eda49c8cb12acdb4d16870da4abd22c5c
TheMiki666 <178334893+TheMiki666@users.noreply.github.com>
2024-12-31 17:40:16 +0100

    Do Pawn#can_move? and test it

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -5,5 +5,91 @@ module Chess
   class Pawn < Chess::Piece
     attr_reader :status
 
+    def initialize(color, col, row)
+      super(color, col, row)
+      @status = 0 #0 = not moved yet, 1 = already moved, 2 = en_passant
+    end
+
+    def remove_en_passant
+        @status = 1 if @status == 2
+    end
+
+    def can_move?(col, row, board)
+      filter_square(col, row)
+      return false if col == @col && row == @row  #Same square
+      if col == @col # Advance pawn
+        can_advance?(col, row, board)
+      elsif (col - @col ).abs == 1 # Adjacent cols => capturing
+        can_capture?(col, row, board)
+      else
+        return false
+      end
+    end
+
+    private
+
+    def can_advance?(col, row, board)
+      if @color == 0
+        if row == @row + 1
+          return (free_square?(col, row, board) == true && free_square?(col, row, board) != 'e')
+        elsif @row == 2 && row == 4
+          a = free_square?(col, 4, board)
+          b = free_square?(col, 3, board)
+          return a == true && a != 'e' && b == true && b != 'e'
+        else
+          false
+        end
+      else #@color ==1
+        if row == @row - 1
+          return (free_square?(col, row, board) == true && free_square?(col, row, board) != 'e')
+        elsif @row == 7 && row == 5
+          a = free_square?(col, 5, board)
+          b = free_square?(col, 6, board)
+          return a == true && a != 'e' && b == true && b != 'e'
+        else
+          false
+        end
+      end
+    end
+
+    def can_capture?(col, row, board)
+      if @color == 0 
+        if row == @row + 1
+          if free_square?(col, row, board) == 'e'
+            true
+          elsif @row == 5 && free_square?(col, row, board) == true
+            #Possible capture en_passant
+            can_capture_en_passant?(col, 5, board)
+          else
+            false
+          end
+        else
+          false
+        end
+      else #@color == 1
+        if row == @row - 1
+          if free_square?(col, row, board) == 'e'
+            true
+          elsif @row == 4 && free_square?(col, row, board) == true 
+            #Possible capture en_passant
+            can_capture_en_passant?(col, 4, board)
+          else
+            false
+          end
+        else
+          false
+        end
+      end
+    end
+
+    def can_capture_en_passant?(col, row, board)
+      enemy = board.get_piece(col, row)
+      if enemy.is_a?(Pawn) && enemy.color != @color && enemy.status == 2
+        true #en passant
+      else
+        false
+      end
+    end
+
   end
 end
---
+++ tests/pawn_test.rb
@@ -0,0 +1,292 @@
+require_relative "../lib/board"
+require_relative "../lib/pawn"
+
+describe Chess::Pawn do
+  describe "white Pawn#can_move? on an empty board from line 2 " do
+    board = Chess::Board.new
+    board.spawn_new_piece('p', 0, 6, 2)
+    pawn = board.get_piece(6, 2)
+    it "Can advance 1 square" do
+      expect(pawn.can_move?(6, 3, board)).to be(true)
+    end
+    it "Can advance 2 squares" do
+      expect(pawn.can_move?(6, 4, board)).to be(true)
+    end
+    it "Can't capture right" do
+      expect(pawn.can_move?(7, 3, board)).to be(false)
+    end
+    it "Can't capture left" do
+      expect(pawn.can_move?(5, 3, board)).to be(false)
+    end
+    it "Can't go back" do
+      expect(pawn.can_move?(6, 1, board)).to be(false)
+    end
+    it "Can't change column" do
+      expect(pawn.can_move?(5, 2, board)).to be(false)
+    end
+    it "Can't stay in the same place" do
+      expect(pawn.can_move?(6, 2, board)).to be(false)
+    end
+    it "Can't advance 3 squares" do
+      expect(pawn.can_move?(6, 5, board)).to be(false)
+    end
+    it "Can't move randomly" do
+      expect(pawn.can_move?(1, 1, board)).to be(false)
+    end
+    it "Rises a error when moving('PQ',3)" do
+      expect{pawn.can_move?('PQ', 3, board)}.to raise_error(TypeError)
+    end
+  end
+
+  describe "black Pawn#can_move? on an empty board from line 7 " do
+    board = Chess::Board.new
+    board.spawn_new_piece('p', 1, 6, 7)
+    pawn = board.get_piece(6, 7)
+    it "Can advance 1 square" do
+      expect(pawn.can_move?(6, 6, board)).to be(true)
+    end
+    it "Can advance 2 squares" do
+      expect(pawn.can_move?(6, 5, board)).to be(true)
+    end
+    it "Can't capture right" do
+      expect(pawn.can_move?(7, 6, board)).to be(false)
+    end
+    it "Can't capture left" do
+      expect(pawn.can_move?(5, 6, board)).to be(false)
+    end
+    it "Can't go back" do
+      expect(pawn.can_move?(6, 8, board)).to be(false)
+    end
+    it "Can't change column" do
+      expect(pawn.can_move?(5, 7, board)).to be(false)
+    end
+    it "Can't stay in the same place" do
+      expect(pawn.can_move?(6, 7, board)).to be(false)
+    end
+    it "Can't advance 3 squares" do
+      expect(pawn.can_move?(6, 4, board)).to be(false)
+    end
+    it "Can't move randomly" do
+      expect(pawn.can_move?(1, 1, board)).to be(false)
+    end
+    it "Rises a error when moving('PQ',3)" do
+      expect{pawn.can_move?('PQ', 3, board)}.to raise_error(TypeError)
+    end
+  end
+
+  describe "white Pawn#can_move? on an empty board from line 3 " do
+    board = Chess::Board.new
+    board.spawn_new_piece('p', 0, 6, 3)
+    pawn = board.get_piece(6, 3)
+    it "Can advance 1 square" do
+      expect(pawn.can_move?(6, 4, board)).to be(true)
+    end
+    it "Can't advance 2 squares" do
+      expect(pawn.can_move?(6, 5, board)).to be(false)
+    end
+  end
+
+  describe "black Pawn#can_move? on an empty board from line 6 " do
+    board = Chess::Board.new
+    board.spawn_new_piece('p', 1, 6, 6)
+    pawn = board.get_piece(6, 6)
+    it "Can advance 1 square" do
+      expect(pawn.can_move?(6, 5, board)).to be(true)
+    end
+    it "Can't advance 2 squares" do
+      expect(pawn.can_move?(6, 4, board)).to be(false)
+    end
+  end
+
+  describe "white Pawn#can_move? on with enemies from line 2 " do
+    board = Chess::Board.new
+    board.spawn_new_piece('p', 0, 6, 2)
+    pawn = board.get_piece(6, 2)
+    board.spawn_new_piece('r', 1, 5, 3)
+    board.spawn_new_piece('r', 1, 6, 3)
+    board.spawn_new_piece('r', 1, 7, 3)
+    board.spawn_new_piece('r', 1, 5, 1)
+    board.spawn_new_piece('r', 1, 6, 1)
+    board.spawn_new_piece('r', 1, 7, 1)
+    board.spawn_new_piece('r', 1, 5, 2)
+    board.spawn_new_piece('r', 1, 7, 2)
+    it "Can't advance 1 square" do
+      expect(pawn.can_move?(6, 3, board)).to be(false)
+    end
+    it "Can't advance 2 squares" do
+      expect(pawn.can_move?(6, 4, board)).to be(false)
+    end
+    it "Can capture right" do
+      expect(pawn.can_move?(7, 3, board)).to be(true)
+    end
+    it "Can capture left" do
+      expect(pawn.can_move?(5, 3, board)).to be(true)
+    end
+    it "Can't move backward" do
+      expect(pawn.can_move?(6, 1, board)).to be(false)
+    end
+    it "Can't capture right backward" do
+      expect(pawn.can_move?(7, 1, board)).to be(false)
+    end
+    it "Can't capture left backward" do
+      expect(pawn.can_move?(5, 1, board)).to be(false)
+    end
+    it "Can't capture right side" do
+      expect(pawn.can_move?(7, 2, board)).to be(false)
+    end
+    it "Can't capture left side" do
+      expect(pawn.can_move?(5, 2, board)).to be(false)
+    end
+  end
+
+  describe "white Pawn#can_move? on with allies from line 2 " do
+    board = Chess::Board.new
+    board.spawn_new_piece('p', 0, 6, 2)
+    pawn = board.get_piece(6, 2)
+    board.spawn_new_piece('r', 0, 5, 3)
+    board.spawn_new_piece('r', 0, 6, 3)
+    board.spawn_new_piece('r', 0, 7, 3)
+    board.spawn_new_piece('r', 0, 5, 1)
+    board.spawn_new_piece('r', 0, 6, 1)
+    board.spawn_new_piece('r', 0, 7, 1)
+    board.spawn_new_piece('r', 0, 5, 2)
+    board.spawn_new_piece('r', 0, 7, 2)
+    it "Can't advance 1 square" do
+      expect(pawn.can_move?(6, 3, board)).to be(false)
+    end
+    it "Can't advance 2 squares" do
+      expect(pawn.can_move?(6, 4, board)).to be(false)
+    end
+    it "Can't capture right" do
+      expect(pawn.can_move?(7, 3, board)).to be(false)
+    end
+    it "Can't capture left" do
+      expect(pawn.can_move?(5, 3, board)).to be(false)
+    end
+    it "Can't move backward" do
+      expect(pawn.can_move?(6, 1, board)).to be(false)
+    end
+    it "Can't capture right backward" do
+      expect(pawn.can_move?(7, 1, board)).to be(false)
+    end
+    it "Can't capture left backward" do
+      expect(pawn.can_move?(5, 1, board)).to be(false)
+    end
+    it "Can't capture right side" do
+      expect(pawn.can_move?(7, 2, board)).to be(false)
+    end
+    it "Can't capture left side" do
+      expect(pawn.can_move?(5, 2, board)).to be(false)
+    end
+  end
+
+  describe "black Pawn#can_move? on with enemies from line 7 " do
+    board = Chess::Board.new
+    board.spawn_new_piece('p', 1, 6, 7)
+    pawn = board.get_piece(6, 7)
+    board.spawn_new_piece('r', 0, 5, 6)
+    board.spawn_new_piece('r', 0, 6, 6)
+    board.spawn_new_piece('r', 0, 7, 6)
+    board.spawn_new_piece('r', 0, 5, 8)
+    board.spawn_new_piece('r', 0, 6, 8)
+    board.spawn_new_piece('r', 0, 7, 8)
+    board.spawn_new_piece('r', 0, 5, 7)
+    board.spawn_new_piece('r', 0, 7, 7)
+    it "Can't advance 1 square" do
+      expect(pawn.can_move?(6, 6, board)).to be(false)
+    end
+    it "Can't advance 2 squares" do
+      expect(pawn.can_move?(6, 5, board)).to be(false)
+    end
+    it "Can capture right" do
+      expect(pawn.can_move?(7, 6, board)).to be(true)
+    end
+    it "Can capture left" do
+      expect(pawn.can_move?(5, 6, board)).to be(true)
+    end
+    it "Can't move backward" do
+      expect(pawn.can_move?(6, 8, board)).to be(false)
+    end
+    it "Can't capture right backward" do
+      expect(pawn.can_move?(7, 8, board)).to be(false)
+    end
+    it "Can't capture left backward" do
+      expect(pawn.can_move?(5, 8, board)).to be(false)
+    end
+    it "Can't capture right side" do
+      expect(pawn.can_move?(7, 7, board)).to be(false)
+    end
+    it "Can't capture left side" do
+      expect(pawn.can_move?(5, 7, board)).to be(false)
+    end
+  end
+
+  describe "black Pawn#can_move? on with allies from line 7 " do
+    board = Chess::Board.new
+    board.spawn_new_piece('p', 1, 6, 7)
+    pawn = board.get_piece(6, 7)
+    board.spawn_new_piece('r', 1, 5, 6)
+    board.spawn_new_piece('r', 1, 6, 6)
+    board.spawn_new_piece('r', 1, 7, 6)
+    board.spawn_new_piece('r', 1, 5, 8)
+    board.spawn_new_piece('r', 1, 6, 8)
+    board.spawn_new_piece('r', 1, 7, 8)
+    board.spawn_new_piece('r', 1, 5, 7)
+    board.spawn_new_piece('r', 1, 7, 7)
+    it "Can't advance 1 square" do
+      expect(pawn.can_move?(6, 6, board)).to be(false)
+    end
+    it "Can't advance 2 squares" do
+      expect(pawn.can_move?(6, 5, board)).to be(false)
+    end
+    it "Can't capture right" do
+      expect(pawn.can_move?(7, 6, board)).to be(false)
+    end
+    it "Can't capture left" do
+      expect(pawn.can_move?(5, 6, board)).to be(false)
+    end
+    it "Can't move backward" do
+      expect(pawn.can_move?(6, 8, board)).to be(false)
+    end
+    it "Can't capture right backward" do
+      expect(pawn.can_move?(7, 8, board)).to be(false)
+    end
+    it "Can't capture left backward" do
+      expect(pawn.can_move?(5, 8, board)).to be(false)
+    end
+    it "Can't capture right side" do
+      expect(pawn.can_move?(7, 7, board)).to be(false)
+    end
+    it "Can't capture left side" do
+      expect(pawn.can_move?(5, 7, board)).to be(false)
+    end
+  end
+
+  describe "white Pawn#can_move? on with enemies from line 6 " do
+    board = Chess::Board.new
+    board.spawn_new_piece('p', 0, 6, 6)
+    pawn = board.get_piece(6, 6)
+    board.spawn_new_piece('r', 1, 5, 7)
+    board.spawn_new_piece('r', 1, 7, 7)
+    it "Can capture right" do
+      expect(pawn.can_move?(7, 7, board)).to be(true)
+    end
+    it "Can capture left" do
+      expect(pawn.can_move?(5, 7, board)).to be(true)
+    end
+  end
+
+  describe "white Pawn#can_move? on with enemies from line 3 " do
+    board = Chess::Board.new
+    board.spawn_new_piece('p', 1, 6, 3)
+    pawn = board.get_piece(6, 3)
+    board.spawn_new_piece('r', 0, 5, 2)
+    board.spawn_new_piece('r', 0, 7, 2)
+    it "Can capture right" do
+      expect(pawn.can_move?(5, 2, board)).to be(true)
+    end
+    it "Can capture left" do
+      expect(pawn.can_move?(7, 2, board)).to be(true)
+    end
+  end
+end
