c81d9a851b956543b664ae1ca607389c46463f58
TheMiki666 <178334893+TheMiki666@users.noreply.github.com>
2025-01-02 07:31:24 +0100

    Do and test more methods of UserInterface
    New methods:
     #ask_for_movement
     #ask_for_promotion
     #message
     #error_message
     #check_message
     #mate_message
     #instructions

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -1,4 +1,5 @@
 require_relative "board"
+require "colorize"
 
 module Chess
   class UserInterface
@@ -7,6 +8,25 @@ module Chess
     def initialize(board, game_manager)
       @board = board
       @game_manager = game_manager
+    end
+
+    def error_message(message)
+      puts message.colorize(:red)
+    end
+
+    def message(message)
+      puts message
+    end
+
+    def check_message
+      puts ("Player #{player}, you're in check!").colorize(:yellow)
+    end
+
+    def mate_message
+      puts ("Player #{player}, you're in check mate!").colorize(:red)
+      @board.switch_turn
+      puts ("Player #{player} wins the match!").colorize(:green)
+      @board.switch_turn
     end
     
     #TESTED
@@ -26,7 +46,84 @@ module Chess
       end
     end
 
+    #TESTED
+    def ask_for_movement
+      loop do
+        answer = 1
+        puts "Player #{player}, is your turn."
+        puts "Enter your movement:"
+        input = gets.chomp.strip.downcase
+        puts
+        if input == 'log'
+          puts "Calling log" #TODO: Replace for the function call
+          answer = 0
+        elsif input == 'board'
+          @board.draw_board
+          answer = 0
+        elsif input == 'save'
+          puts "Calling save" #TODO: Replace for the function call
+          answer = 0
+        elsif input == 'load'
+          puts "Calling load" #TODO: Replace for the function call
+          answer = 0
+        elsif input == 'resign'
+          puts "Calling resign" #TODO: Replace for the function call
+        elsif input == 'quit'
+          puts "Calling quit" #TODO: Replace for the function call
+        else
+          answer = parse_input(input)
+        end
+        return answer if !answer.nil? && answer !=0 
+        if answer.nil?
+          puts "Input not correct".colorize(:red)
+          instructions
+        end
+      end
+    end
+
+    #TESTED
+    def instructions
+      puts "Enter your movements with algebraic notation."
+      puts "For example: c1-d4"
+      puts "It is accepted, but not necessary, the capture symbol:"
+      puts "c1xd4"
+      puts "...or check symbol:"
+      puts "c1xd4+"
+      puts "or even castling notation:"
+      puts "O-O or O-O-O" 
+      puts "Enter 'save' to save the match."
+      puts "Enter 'load' to load a saved match."
+      puts "Enter 'board' to see the board"
+      puts "Enter 'log' to see the log of the match moves"
+      puts "Enter 'resign' to resign the match"
+      puts "Enter 'quit' to leave the program"
+    end
+
+    # TESTED
+    def ask_for_promotion
+      loop do
+        puts "Player #{player}, one of your pawns has ".concat("promoted!").colorize(:green)
+        puts "Which piece do you want to promote to (Queen recommended)?"
+        puts "'Q' = Queen, 'R' = Rook, 'B' = Bishop, 'N' = Knight"
+        answer = gets.chomp.strip.upcase
+        puts
+        return answer if answer == 'Q' || answer == 'R' || answer == 'B' || answer == 'N'
+        if answer == 'P'
+          puts "The pawn can not promote to another pawn!".colorize(:red)
+        elsif answer == 'K'
+          puts "The pawn can not promote to King!".colorize(:red)
+        else
+          puts "Input not correct".colorize(:red)
+        end
+      end
+    end
+
     private
+
+    def player
+      @board.player_turn == 0 ? "White" : "Black"
+    end
+
     #TESTED
     def extract_movement(string)
       col1 = string[0].ord - ORD_CONSTANT
---
+++ tests/user_interface_test.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/user_interface'
+
+board = Chess::Board.new
+ui = Chess::UserInterface.new(board, nil)
+board.new_match
+
+ui.instructions #ok
+#Test ask for promotion
+#p ui.ask_for_promotion  #Correctly tested
+
+#p ui.ask_for_movement #Correctly tested
+ui.message("Awful movement!") #ok
+ui.error_message("Awful movement!") #ok
+ui.mate_message #ok
+ui.check_message #ok
+board.switch_turn
+ui.mate_message #ok
+ui.check_message #ok
